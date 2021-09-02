0facfdc347051cc253e48dfa024c3dda9d545131
raju katakam <83388334+raju-katakam@users.noreply.github.com>
2021-09-02 14:56:27 +0530

    this is update file
    up to date

---
+++ home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+ 
+@section('content')
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+	<button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+</div>
+@endif
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+ 
+                <div class="card-body">
+                    @if(auth()->user()->is_admin == 1)
+                    <a >welcome Admin</a>
+                    @else
+                    <div class=”panel-heading”>Normal User</div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
