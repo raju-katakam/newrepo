41233f99f6bcaf43e58026e8cd6d64afb73c81fa
raju <raju.coderkube@gmail.com>
2021-09-02 12:26:05 +0530

    This is my first commit!

---
+++ addnew.blade.php
@@ -0,0 +1,78 @@
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Add new') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{url('/addnew') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus>
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autocomplete="description">
+
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="Expire Date" class="col-md-4 col-form-label text-md-right">{{ __('Expire Date') }}</label>
+
+                            <div class="col-md-6">
+                            <select name="Expire_Date">
+                            <option value="24">24h</option> 
+                            <option value="48">48h</option>
+                            <option value="72">72h</option>
+                            </select>
+
+                                @error('Expire Date')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for=" images" class="col-md-4 col-form-label text-md-right">{{ __(' images') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="images" type="file" class="form-control" name="images" >
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Add') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ admin.blade.php
@@ -0,0 +1,18 @@
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+ 
+                <div class="card-body">
+                    @if(auth()->user()->is_admin == 1)
+                    <a >Admin</a>
+                    @else
+                    <div class=”panel-heading”>Normal User</div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
